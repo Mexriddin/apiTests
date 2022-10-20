df86cd9485b4acbab599e7d2976882158c92f4b9
Mexriddin <maxem2019@gmail.com>
2022-10-20 10:52:48 +0500

    add registration test in allure report

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,11 +1,15 @@
 import pytest
+import allure
 
 from utils.base_case import BaseCase
 from utils.assertions import Assertions
 from utils.my_requests import MyRequests
 
 
+@allure.epic("Registration cases")
 class TestUserRegister(BaseCase):
+    @allure.title("Positive test registration user")
+    @allure.description("This test successfully registrate user by valid fields")
     @pytest.mark.positive
     @pytest.mark.smoke
     def test_create_user_successfully(self):
@@ -14,6 +18,8 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response=response, expected_status_code=200)
         Assertions.assert_json_has_key(response=response, name="id")
 
+    @allure.title("Negative test registration user with existing email")
+    @allure.description("This test checks registration status and content with sending existing email")
     @pytest.mark.negative
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
@@ -23,6 +29,8 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
             f"Unexpected response content {response.content} "
 
+    @allure.title("Negative test registration user with invalid email")
+    @allure.description("This test checks registration status and content with sending email without letter '@'")
     @pytest.mark.negative
     def test_create_user_with_invalid_email(self):
         invalid_email = 'vinkotovexample.com'
@@ -32,6 +40,9 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == "Invalid email format", \
             f"Unexpected response content {response.content} "
 
+    @allure.title("Negative test registration user without specifying one of the fields")
+    @allure.description("This test checks registration status and content with sending without "
+                        "specifying one of the fields")
     @pytest.mark.negative
     @pytest.mark.parametrize("without_field", BaseCase.data.keys(), ids=[
         'without password',
@@ -46,6 +57,8 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"The following required params are missed: {without_field}", \
             f"Unexpected response content {response.content} "
 
+    @allure.title("Negative test registration user with invalid username")
+    @allure.description("This test checks registration status and content with sending invalid username")
     @pytest.mark.negative
     @pytest.mark.parametrize("username,error_message", [
         ('a', "The value of 'username' field is too short"),
@@ -57,4 +70,3 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response=response, expected_status_code=400)
         assert response.content.decode("utf-8") == f"{error_message}", \
             f"Unexpected response content {response.content} "
-
